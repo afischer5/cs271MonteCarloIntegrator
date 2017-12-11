2fdd6f82860b34afa8f99348ed9a98bd5f6b78a9
Andrew Fischer <afischer5@luc.edu>
2017-12-11 18:42:25 +0000

    Integrator is working well and has some minor usabliity upgrades. Needs to be made more user friendly and doesnt run unless using ./gradlew run for some reason. blame it on dependencies.

--- src/main/java/edu/luc/cs271/montecarlo/Main.java
+++ src/main/java/edu/luc/cs271/montecarlo/Main.java
@@ -10,7 +10,7 @@ public class Main {
      /* This main class is the driver for asking for user information and interpreting it
      as well as displaying the results of an asked for calculation*/
      
-     //Scanner keyboard = new Scanner(System.in).useDelimiter(",");
+     
      Scanner keyboard = new Scanner(System.in);
      String input;
      Expression exp;
@@ -18,35 +18,47 @@ public class Main {
      List<Double> mins = new ArrayList<Double>();
      List<Double> maxs = new ArrayList<Double>();
      
-     System.out.println("Monte Carlo Integrator for instructions enter 'help' or '?'. To terminate please enter 'terminate'.");
+     System.out.println("Monte Carlo Integrator for instructions enter 'help' or '?'. To terminate please enter 'terminate'. To integrate enter 'integrate' ");
      
      do{
-      input = keyboard.nextLine();
+       input = keyboard.nextLine();
+     
+      if(input.equalsIgnoreCase("?") || input.equalsIgnoreCase("help")) {
+          System.out.println("Please enter a mathematical function. Use all caps and shorthand form for trig functions and ABS for absolute value");
+          System.out.println("Follow this function up with a range of numbers seperated by a space that indicates respectively the min of range, the max of range, the number of points to use.");
+          System.out.println("Follow this with a list of variables to integrate with each seperated by a space.");
+          System.out.println("Then enter a domain for the integration bounds for each variable in the integrand seperated by a space. i.e. Create a pair of numbers for each variable and sperate each pair by a space aswell.");
+          input = keyboard.nextLine();
+      } 
+     
+    
       
       //the follow is displayed when teh user asks for help
       while(input.equalsIgnoreCase("?") || input.equalsIgnoreCase("help")) {
           System.out.println("Please enter a mathematical function. Use all caps and shorthand form for trig functions and ABS for absolute value");
           System.out.println("Follow this function up with a range of numbers seperated by a space that indicates respectively the min of range, the max of range, the number of points to use.");
-          System.out.println("Follow this with a list of variables to integrate with each seperated by a comma.");
-          System.out.println("Then enter a domain for the integration bounds for each variable in the integrand seperated by a comma. i.e. Create a pair of numbers for each variable.");
+          System.out.println("Follow this with a list of variables to integrate with each seperated by a space.");
+          System.out.println("Then enter a domain for the integration bounds for each variable in the integrand seperated by a space. i.e. Create a pair of numbers for each variable and sperate each pair by a space aswell.");
           input = keyboard.nextLine();
       } 
       
-      exp = new Expression(input);
+      if(input.equalsIgnoreCase("integrate")){
+       
+     
+      exp = new Expression(keyboard.nextLine());
       System.out.println(input);
       Double rMin = keyboard.nextDouble();
       Double rMax = keyboard.nextDouble();
       int numberPoints = keyboard.nextInt();
       keyboard.nextLine();
-      System.out.println(rMin + rMax);
       
       String variableString = keyboard.nextLine();
-       List<String> vars = Arrays.asList(variableString.split(","));
+       List<String> vars = Arrays.asList(variableString.split(" "));
        for(int i = 0; i < vars.size(); i ++){
           variables.add(vars.get(i));
        }
       
-     List<String> domains = Arrays.asList(keyboard.nextLine().split(","));
+     List<String> domains = Arrays.asList(keyboard.nextLine().split(" "));
       for(int i = 0; i < domains.size(); i= i +2){
           mins.add(Double.parseDouble(domains.get(i)));
           maxs.add(Double.parseDouble(domains.get(i + 1)));
@@ -54,25 +66,11 @@ public class Main {
       
       
       Integrator integrator = new Integrator(exp, variables, numberPoints,rMin,rMax);
-      System.out.println(integrator.integrate(mins,maxs));
+      System.out.println(exp + " integrated over " + variableString +" with respective domains " + domains + " = " +integrator.integrate(mins,maxs));
       
-      
-     } while(!input.equalsIgnoreCase("terminate"));
+      }
+     }while(!input.equalsIgnoreCase("terminate")); 
      
-     
-        
-        
-        
-        /*Expression exp = new Expression("1 + x");
-        List<String> vars = new ArrayList<>();
-        vars.add("x");
-         List<Double> mins = new ArrayList<>();
-         mins.add(2.0);
-         List<Double> maxs = new ArrayList<>();
-         maxs.add(2.1);
-        Integrator integrator = new Integrator(exp,vars);
-        System.out.println(integrator.integrate(mins,maxs));*/
-        
         keyboard.close();
     }
 }
