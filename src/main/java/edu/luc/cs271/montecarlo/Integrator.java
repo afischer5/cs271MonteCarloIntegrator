38799fcb681833d76b9848f0a2b686edee4348f3
Andrew Fischer <afischer5@luc.edu>
2017-12-11 00:55:25 +0000

    integrator nearly finished need to see if we can push variable values to expression dynamically

--- src/main/java/edu/luc/cs271/montecarlo/Integrator.java
+++ src/main/java/edu/luc/cs271/montecarlo/Integrator.java
@@ -4,7 +4,7 @@ import com.udojava.evalex.Expression;
 
 //this class handles all checks to see if a point is whithin a functions bounds after creating them and stores information about the function
 public class Integrator {
-  String exp;
+  Expression exp;
   double minRange;
   double maxRange;
   PointGenerator pointGen;
@@ -15,7 +15,7 @@ public class Integrator {
   final double DEFAULT_MIN_RANGE = -100;
   final int DEFAULT_NUMBER_OF_POINTS = 10000;
   
-  public Integrator(String exp, List<String> vars, int numberOfPoints, double min, double max){
+  public Integrator(Expression exp, List<String> vars, int numberOfPoints, double min, double max){
       this.exp = exp;
       this.variables = vars;
       pointGen = new PointGenerator(vars.size());
@@ -24,7 +24,8 @@ public class Integrator {
       this.numberOfPoints = numberOfPoints;
   }
   
-  public Integrator(String exp, List<String> vars){
+  //a construtor but with several values set to default for ease of construction
+  public Integrator(Expression exp, List<String> vars){
       this.exp = exp;
       this.variables = vars;
       pointGen = new PointGenerator(vars.size());
@@ -33,10 +34,41 @@ public class Integrator {
       this.numberOfPoints = DEFAULT_NUMBER_OF_POINTS;
   }
   
-  public double integrate (List<Double> mins, List<Double> maxs, List<String> variables){
+  //does the actual calculation of the integral based on information stored in
+  //integrator seperating like this alows us to do the same integral but with a
+  //different domain easily
+  public double integrate (List<Double> mins, List<Double> maxs){
+      double result = 0;
+      
       for(int i = 0; i < this.numberOfPoints; i++){
+          pointGen.generatePoint(mins,maxs,this.minRange,this.maxRange);
           
+          //the zero independent variables case
+          /*if(dim == 0){
+              final double value = this.exp.eval().doubleValue();
+              
+              if(value < 0){
+                  if(pointGen.getPointCoords().get(0) > value) result--;
+              }
+               if(value > 0){
+                  if(pointGen.getPointCoords().get(0) < value) result++;
+              }
+          }//end zero independent case*/
+          
+          //inserts all the variable values into the expression
+          for(int j = 0; j < this.variables.size(); j++){
+              this.exp = this.exp.with(this.variables.get(j),Double.toString(pointGen.getPointCoords().get(j+1)));
+          }
+          
+          value = this.exp.eval();
+          independentVariable = pointGen.getPointCoords.get(0);
+          
+          //determines if the point is within the integrand
+          if(value < 0 && value < independentVariable) result--;
+          if(value > 0 && value > independentVariable) result++;
       }
+      
+      
       return 6.7;
   }
   
--- src/main/java/edu/luc/cs271/montecarlo/PointGenerator.java
+++ src/main/java/edu/luc/cs271/montecarlo/PointGenerator.java
@@ -12,25 +12,33 @@ public class PointGenerator { //maybe we should make this a point and put point 
     this.coordinates = new ArrayList<Double>();
   }
   
-
+  //dim returns the number of independen dimensions of integration
   public int getDim(){
     return this.dimension;
   }
   
   //dim defines the number of dimensions a random double between max and min is randomly generated for each point
-  public void generatePoint(List<Double> min, List<Double> max){
+  public void generatePoint(List<Double> min, List<Double> max, Double rMin, Double rMax){
     if(min.size()!=this.dimension || max.size()!=this.dimension) throw new IllegalArgumentException("min and max list have to have same size as the dimension of the point");
     
     Random generator = new Random();
     
+    //this adds our dependent variable
+    this.coordinates.add(generator.nextDouble() * (rMax - rMin) + rMin);
+    
+    //adds each of our independent variables
     for(int i = 0; i < this.dimension; i++){
-      this.coordinates.add(i,generator.nextDouble() * (max.get(i) - min.get(i)) + min.get(i));
+      this.coordinates.add(generator.nextDouble() * (max.get(i) - min.get(i)) + min.get(i));
     }
-    
-    
   }
   
   
-  
-  
+  public List<Double> getPointCoords(){
+      return this.coordinates;
+  }
+    
 }
+  
+  
+  
+  
